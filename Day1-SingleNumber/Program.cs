d91cb4ef824a428b0bb7db9dc62ad045b75cc765
udubnate <natebruneau@gmail.com>
2020-04-01 09:36:34 -0700

    Testing new username

--- Day1-SingleNumber/Program.cs
+++ Day1-SingleNumber/Program.cs
@@ -6,7 +6,7 @@ namespace Day1_SingleNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Change Text!");
         }
     }
 }
