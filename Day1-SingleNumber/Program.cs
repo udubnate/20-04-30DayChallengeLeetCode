71e3591de5f43d4eca3751d4dd77bcacf203d326
Nate Bruneau <=>
2020-04-01 09:26:51 -0700

    initial checkin

---
+++ Day1-SingleNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1_SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
