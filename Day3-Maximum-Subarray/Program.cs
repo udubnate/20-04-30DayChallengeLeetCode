bc304431135072b1b606123f210ba0f0b4606398
udubnate <natebruneau@gmail.com>
2020-04-03 09:56:01 -0700

    Accepted code O(n^2), can refactor and do better

---
+++ Day3-Maximum-Subarray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day3_Maximum_Subarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] ints = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            //int[] ints = new int[] { 7, 1, -3, 4, -1, 2, 1, -5, 4 };
+            ints = new int[] { 1 };
+            ints = new int[] { 0 };
+            ints = new int[] { -2, 1 };
+            ints = new int[] { -1 };
+            int output = s.MaxSubArray(ints);
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
---
+++ Day3-Maximum-Subarray/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Day3_Maximum_Subarray
+{
+    public class Solution
+    {
+
+        public int MaxSubArray(int[] nums)
+        {
+            int sum;
+            int highest = int.MinValue;
+
+            for (int i=0; i < nums.Length; i++)
+            {
+                sum = nums[i];
+                
+                highest = getHighest(sum, highest);
+
+                for (int j=i+1; j < nums.Length; j++)
+                {
+                    sum += nums[j];
+                    highest = getHighest(sum, highest);
+                }
+
+            }
+
+            return highest;
+        }
+
+        int getHighest(int n, int m)
+        {
+            if (n > m)
+            {
+                return n;
+            }  else
+            {
+                return m;
+            }
+        }
+    }
+}
