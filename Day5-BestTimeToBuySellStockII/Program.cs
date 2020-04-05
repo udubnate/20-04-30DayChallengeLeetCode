de87138320deee1b802c58f79055186de3643904
udubnate <natebruneau@gmail.com>
2020-04-05 08:57:32 -0700

    surprised to get this one the first try, thought this exercise was going to get me.

---
+++ Day5-BestTimeToBuySellStockII/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day5_BestTimeToBuySellStockII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = new int[] { 7, 1, 5, 3, 6, 4 };
+            //nums = new int[] { 1, 2, 3, 4, 5 };
+            nums = new int[] { 7, 6, 4, 3, 1};
+            int maxprofit = s.MaxProfit(nums);
+            Console.WriteLine(maxprofit.ToString());
+        }
+    }
+}
---
+++ Day5-BestTimeToBuySellStockII/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_BestTimeToBuySellStockII
+{
+    class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            bool buy = true;
+            int profit = 0;
+            int transaction = 0;
+
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                //Check to Buy 
+                if ((prices[i] - prices[i + 1]) < 0 && buy)
+                {
+                    //trigger buy
+                    transaction += prices[i];
+                    buy = false;
+                    Console.WriteLine("Day " + (i + 1).ToString() + " :: Buy");
+                }
+                else if ((prices[i] - prices[i + 1]) > 0 && !buy)
+                {
+                    //trigger sell
+                    transaction = prices[i] - transaction;
+                    profit += transaction;
+                    transaction = 0;
+                    Console.WriteLine("Day " + (i + 1).ToString() + " :: Sell");
+                    buy = true;
+                }
+                //if you are at the last element and you haven't sold yet
+                if (!buy && (i == prices.Length - 2))
+                {
+                    //trigger sell
+                    transaction = prices[i+1] - transaction;
+                    profit += transaction;
+                    transaction = 0;
+                    Console.WriteLine("Day " + (i + 2).ToString() + " :: Sell");
+                    buy = true;
+                }
+            }
+            return profit;
+        }
+
+    }
+}
