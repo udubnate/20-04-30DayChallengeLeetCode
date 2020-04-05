5a7e59656c735181f24e15021918e7ea70ce3ab5
udubnate <natebruneau@gmail.com>
2020-04-05 09:09:14 -0700

    added some better testing where I can run multiple tests at once for future problems

--- Day5-BestTimeToBuySellStockII/Program.cs
+++ Day5-BestTimeToBuySellStockII/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day5_BestTimeToBuySellStockII
 {
@@ -7,11 +8,22 @@ namespace Day5_BestTimeToBuySellStockII
         static void Main(string[] args)
         {
             Solution s = new Solution();
-            int[] nums = new int[] { 7, 1, 5, 3, 6, 4 };
-            //nums = new int[] { 1, 2, 3, 4, 5 };
-            nums = new int[] { 7, 6, 4, 3, 1};
-            int maxprofit = s.MaxProfit(nums);
-            Console.WriteLine(maxprofit.ToString());
+
+            List<int[]> IntArray = new List<int[]>
+            {
+                new int[] { 7, 1, 5, 3, 6, 4 },
+                new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 7, 6, 4, 3, 1}
+            };
+
+            int count = 0;
+            foreach (int[] nums in IntArray){
+                Console.WriteLine("Test # " + count + " [" + String.Join(',', nums) + "] ::");
+                int maxprofit = s.MaxProfit(nums);
+                Console.WriteLine(maxprofit.ToString());
+                count++;
+            }
+            
         }
     }
 }
