7665af642407eed861b5005dfc4dd7de9f25e9f9
udubnate <natebruneau@gmail.com>
2020-04-06 11:03:00 -0700

    This was a tricky, reviewed others code due to C# syntax around iLists and Hashtables/Lists combos

---
+++ Day6-GroupAnagrams/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day6_GroupAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string[] nums = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            s.GroupAnagrams(nums);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Day6-GroupAnagrams/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace Day6_GroupAnagrams
+{
+    class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            //check if item exists
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+
+            for (int i=0; i < strs.Length; i++)
+            {
+                //sort string
+                string sortedstring = String.Concat(strs[i].OrderBy(c => c));
+
+                if (!(dict.ContainsKey(sortedstring)))
+                {
+                    dict.Add(sortedstring, new List<string>());
+                } 
+                dict[sortedstring].Add(strs[i]);
+            }
+            List<IList<String>> results = new List<IList<String>>(dict.Values);
+
+            return results;
+        }
+    }
+}
