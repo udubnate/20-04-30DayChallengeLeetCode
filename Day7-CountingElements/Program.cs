8dd0ab4f4f958720b0fda35e4c994f0a46b7bf61
udubnate <natebruneau@gmail.com>
2020-04-07 10:41:13 -0700

    first attempt, didn't understand the question, impact counter was a good attempt

---
+++ Day7-CountingElements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Day7_CountingElements
+{
+    //Given an integer array arr, count element x such that x + 1 is also in arr.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give an array to test:");
+            String str = Console.ReadLine();
+            int[] nums = str.Split(',').Select(int.Parse).ToArray();
+
+            Solution s = new Solution();
+            Console.WriteLine(s.CountElements(nums).ToString());
+
+        }
+    }
+}
---
+++ Day7-CountingElements/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day7_CountingElements
+{
+    class Solution
+    {
+        public int CountElements(int[] arr)
+        {
+            Array.Sort(arr);
+            Dictionary<int, int> impact = new Dictionary<int, int>();
+            foreach (int num in arr)
+            {
+                if (impact.ContainsKey(num))
+                {
+                    impact[num] += 1;
+                } else
+                {
+                    impact.Add(num, 1);
+                }
+            }
+            int count = 0;
+
+            int[] uniquearr = arr.Distinct().ToArray();
+
+            for (int i = 0; i < uniquearr.Length - 1; i++)
+            {
+                if (uniquearr[i] == (uniquearr[i+1] - 1))
+                {
+                    if (impact[(uniquearr[i])] == impact[(uniquearr[i + 1])])
+                    {
+                        count += impact[(uniquearr[i])];
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
