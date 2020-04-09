def95f14350c698a9004a00b61fc0bb39bec8b2e
udubnate <natebruneau@gmail.com>
2020-04-09 10:01:58 -0700

    Took me a bit longer than anticipated - lots of troubleshooting on the Remove(), but got it completed

---
+++ Day9-BackspaceStringCompare/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day9_BackspaceStringCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string str1 = "xywrrmp";
+            string str2 = "xywrrmu#p";
+            bool compare = s.BackspaceCompare(str1, str2);
+            Console.WriteLine(compare.ToString());
+        }
+    }
+}
+
---
+++ Day9-BackspaceStringCompare/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace Day9_BackspaceStringCompare
+{
+
+//Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+    class Solution
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            S = BacktickString(S);
+            T= BacktickString(T);
+
+            if (S == T)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+     
+    // find # in a String, and delete the # and the previous character.
+    // if the first character, only delete #
+    private string BacktickString(string s) 
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '#')
+                {
+                    if (i == 0)
+                    {
+                        s = s.Remove(i,1);
+                        i--;
+                    } 
+                    else
+                    {
+                        s = s.Remove(i - 1, 2);
+                        i -= 2;
+                    }
+                }
+            }
+            return s;
+        }
+    }
+}
