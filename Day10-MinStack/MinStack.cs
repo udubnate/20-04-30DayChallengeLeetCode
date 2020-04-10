4ce0b25d90795305ea1c9c6990d33377c1aab254
udubnate <natebruneau@gmail.com>
2020-04-10 09:15:10 -0700

    Needed a bit of help for the GetMin, and the solution I came up with I am not super proud about. Might rethink this one later

---
+++ Day10-MinStack/MinStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day10_MinStack
+{
+    public class MinStack
+    {
+        private List<int> arr = new List<int>();
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            arr.Add(x);
+        }
+
+        public void Pop()
+        {
+            arr.RemoveAt(arr.Count-1);
+        }
+
+        public int Top()
+        {
+            return arr[arr.Count-1];
+        }
+
+        public int GetMin()
+        {
+            return arr.Min();
+        }
+    }
+
+}
---
+++ Day10-MinStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day10_MinStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinStack obj = new MinStack();
+            obj.Push(-2);
+            obj.Push(0);
+            obj.Push(-3);
+            int min1 = obj.GetMin(); // Returns - 3.
+            obj.Pop();
+            int top = obj.Top(); // Returns 0.
+            int min2 = obj.GetMin(); // Returns - 2.
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
