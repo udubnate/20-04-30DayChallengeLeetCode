41cd4f13b5d890cf3aae0d004ea11b708b3d6b51
udubnate <natebruneau@gmail.com>
2020-04-12 09:47:45 -0700

    adding Day12

---
+++ Day12-LastStoneWeight/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day12_LastStoneWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = new int[] { 2, 2};
+            Console.WriteLine(s.LastStoneWeight(nums).ToString());
+        }
+    }
+}
---
+++ Day12-LastStoneWeight/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day12_LastStoneWeight
+{
+    public class Solution
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            //special case for stones = 1
+            if (stones.Length == 1)
+            {
+                return stones[0];
+            }
+
+            List<int> stoneList = stones.OfType<int>().ToList();
+
+            do
+            {
+                SmashStones(stoneList);
+            }
+            while (stoneList.Count > 1);
+
+            // if stones are empty, return 0 otherwise return the remainder number
+            if (stoneList.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return stoneList[0];
+            }
+        }
+
+        private void SmashStones(List<int> stones)
+        {
+            List<int> sortedStones = new List<int>(stones);
+            sortedStones.Sort();
+            int largest = sortedStones[sortedStones.Count - 1];
+            int second = sortedStones[sortedStones.Count - 2];
+
+            if (second == largest)
+            {
+                stones.Remove(second);
+                stones.Remove(largest);
+            }
+            else
+            {
+                stones.Remove(second);
+                int largeindex = stones.IndexOf(largest);
+                stones[largeindex] = largest - second;
+            }
+
+        }
+    }
+}
