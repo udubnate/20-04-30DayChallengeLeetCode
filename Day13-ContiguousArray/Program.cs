4bf95763cf25a4fb286103bdeec115fee3397315
udubnate <natebruneau@gmail.com>
2020-04-13 09:08:12 -0700

    here is my O(n^2) solution. exceeds timeout

---
+++ Day13-ContiguousArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day13_ContiguousArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //int[] nums = new int[] { 1,1,0,0,1,1,0 };
+            //int[] nums = new int[] { 1, 0 };
+            int[] nums = new int[] { 0, 0, 1, 0, 0, 0, 1, 1 };
+            Console.WriteLine(s.FindMaxLength(nums).ToString());
+        }
+    }
+}
---
+++ Day13-ContiguousArray/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Day13_ContiguousArray
+{
+
+    //Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+    public class Solution
+    {
+        public int FindMaxLength(int[] nums)
+        {
+            List<int> intList = nums.ToList();
+
+            int counter = 0;
+
+            int maxvalue = 0;
+            
+            for (int i =0; i < intList.Count - 1; i++)
+            {
+                for (int j = i; j < intList.Count; j++)
+                {
+                    if (intList[j] == 0)
+                    {
+                        counter--;
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+
+                    if (counter == 0)
+                    {
+                        maxvalue = Math.Max(maxvalue, j - i + 1);
+                    }
+                }
+
+                counter = 0;
+                  
+
+            }
+
+            return maxvalue;
+        }
+    }
+}
