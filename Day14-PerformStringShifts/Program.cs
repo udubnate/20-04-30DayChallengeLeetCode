beb566ebb28022a0dc93407eee79b8f6a86bc0b8
udubnate <natebruneau@gmail.com>
2020-04-14 14:17:10 -0700

    got this one with a helper article for the rotation

---
+++ Day14-PerformStringShifts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day14_PerformStringShifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            int[][] shift =
+            {
+                new int[] {1,1},
+                new int[] {1,1},
+                new int[] {0,2},
+                new int[] {1,3}
+            };
+            string str = s.StringShift("abcdefg", shift);
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Day14-PerformStringShifts/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_PerformStringShifts
+{
+    class Solution
+    {
+
+        // helper article: https://www.geeksforgeeks.org/left-rotation-right-rotation-string-2/
+
+        public string StringShift(string s, int[][] shift)
+        {
+            foreach (int[] num in shift)
+            {
+                if (num[0] == 0)
+                {
+                    s = leftshift(s, num[1]);
+                }
+                else
+                {
+                    s = rightshift(s, num[1]);
+                }
+            }
+
+            return s;
+        }
+
+        public string leftshift(string s, int d)
+        {
+            return (s.Substring(d, s.Length - d) + s.Substring(0, d));
+        }
+
+        public string rightshift(string s, int d)
+        {
+            return leftshift(s, s.Length - d);
+        }
+    }
+}
