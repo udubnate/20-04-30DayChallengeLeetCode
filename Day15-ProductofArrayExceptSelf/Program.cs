3ad3049869cc0bbad1f9767a190f55f10d718e5a
udubnate <natebruneau@gmail.com>
2020-04-15 11:32:08 -0700

    using LINQ, didn't work with time exceeded

---
+++ Day15-ProductofArrayExceptSelf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Day15_ProductofArrayExceptSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input:  [1,2,3,4]
+            //Output: [24, 12, 8, 6]
+            string input = "[1,2,3,4]";
+            int[] nums = getIntArray(input);
+            Solution s = new Solution();
+            int[] output = s.ProductExceptSelf(nums);
+            string str = String.Join(',', output);
+            Console.WriteLine(str);
+        }
+
+        static int[] getIntArray(string str)
+        {
+            str = str.Trim('[', ']');
+            int[] nums = str.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+            return nums;
+        }
+    }
+}
---
+++ Day15-ProductofArrayExceptSelf/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day15_ProductofArrayExceptSelf
+{
+    class Solution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            List<int> outList = new List<int>();
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                List<int> tempArray = new List<int>();
+                tempArray = nums.ToList<int>();
+                tempArray.RemoveAt(i);
+
+                var product = tempArray.Aggregate((total, next) => total * next);
+                outList.Add(product);
+            }
+            return outList.ToArray();
+        }
+    }
+}
