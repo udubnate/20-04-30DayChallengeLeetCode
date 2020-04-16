3c638af8fcdc77f9b7b5c269e36b7cbdc9c8a37e
udubnate <natebruneau@gmail.com>
2020-04-16 10:16:09 -0700

    my first attempt, failed test case )(

---
+++ Day16-Valid-Parenthesis-String/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day16_Valid_Parenthesis_String
+{
+    class Program
+    {
+        static Dictionary<string, bool> testCases;
+
+        static void Main(string[] args)
+        {
+            
+            testCases = new Dictionary<string, bool>();
+            //positive tests
+            AddTest("()()");
+            AddTest("((*)");
+            AddTest("(*)*");
+            AddTest("((()))");
+            AddTest("(*))");
+            //negative tests
+            AddTest("(()");
+            AddTest("");
+
+
+            foreach (KeyValuePair<string,bool> kvp in testCases)
+            {
+                Console.WriteLine("Test: " + kvp.Key + ", Value = " + kvp.Value);
+            }
+        }
+
+        static void AddTest(string str)
+        {
+            Solution s = new Solution();
+            bool test1 = s.CheckValidString(str);
+            testCases.Add(str, test1);
+        }
+    }
+}
---
+++ Day16-Valid-Parenthesis-String/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day16_Valid_Parenthesis_String
+{
+    public class Solution
+    {
+
+        public bool CheckValidString(string s)
+        {
+            // check for empty string
+            if (s.Length == 0) { return false;  }
+
+            int balance = 0;
+            int ast_count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    balance -= 1;
+                }
+                else if(s[i] == ')')
+                {
+                    balance += 1;
+                }
+                else if(s[i] == '*')
+                {
+                    ast_count++;
+                }
+            }
+
+            //asterisk gives you a balance negotiation 
+            if (Math.Abs(balance) <= ast_count)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+            /*
+            //check matching () return true, else false
+            if (balance == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+            */
+        }
+    }
+}
