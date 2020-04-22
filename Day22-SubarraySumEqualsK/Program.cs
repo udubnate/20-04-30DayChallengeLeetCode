c533bcbdfb090ee90e6f0fd93131fd143e8ba2e2
udubnate <natebruneau@gmail.com>
2020-04-22 16:06:25 -0700

    brute force solution O(n^3)

---
+++ Day22-SubarraySumEqualsK/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace Day22_SubarraySumEqualsK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 1,1,1 };
+            Solution s = new Solution();
+            int output = s.SubarraySum(input, 2);
+
+            Console.WriteLine("Output = " + output);
+        }
+    }
+
+    public class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            int count = 0;
+
+            for (int start = 0; start < nums.Length; start++)
+            {
+                for (int end = start + 1; end <= nums.Length; end++)
+                {
+                    int sum = 0;
+                    for (int i = start; i < end; i++)
+                    {
+                        sum += nums[i];
+                    }
+                    if (sum == k)
+                    {
+                        count++;
+                    }
+                    
+                }
+            }
+            return count;
+
+        }
+    }
+}
